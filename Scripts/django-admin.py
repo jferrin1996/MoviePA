b385fffa10a17a618897e93c42d3ba7c45563ef4
jferrin1996 <jakeferrin1996@gmail.com>
2022-02-23 20:13:36 -0700

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\14022\Documents\college\Masters\Fall 2021\Royce\Movie3\myvenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
